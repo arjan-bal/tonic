602d0beb3c01032a4816ecc5c89b606940944aeb
Arjan Bal <arjansbal@google.com>
2025-07-01 18:52:43 +0530

    Add protoc plugin

---
+++ compiler/src/grpc_rust_generator.h
@@ -0,0 +1,70 @@
+/*
+ *
+ * Copyright 2025 gRPC authors.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ *
+ */
+
+#ifndef NET_GRPC_COMPILER_RUST_GENERATOR_H_
+#define NET_GRPC_COMPILER_RUST_GENERATOR_H_
+
+#include <iostream>
+#include <stdlib.h> // for abort()
+
+#include <google/protobuf/compiler/rust/context.h>
+#include <google/protobuf/descriptor.h>
+#include <google/protobuf/io/zero_copy_stream.h>
+
+class LogHelper {
+  std::ostream *os;
+
+public:
+  LogHelper(std::ostream *os) : os(os) {}
+  [[noreturn]] ~LogHelper() {
+    *os << std::endl;
+    ::abort();
+  }
+  std::ostream &get_os() { return *os; }
+};
+
+// Abort the program after logging the message if the given condition is not
+// true. Otherwise, do nothing.
+#define GRPC_CODEGEN_CHECK(x)                                                  \
+  !(x) && LogHelper(&std::cerr).get_os()                                       \
+              << "CHECK FAILED: " << __FILE__ << ":" << __LINE__ << ": "
+
+// Abort the program after logging the message.
+#define GRPC_CODEGEN_FAIL GRPC_CODEGEN_CHECK(false)
+
+namespace rust_grpc_generator {
+
+namespace impl {
+namespace protobuf = google::protobuf;
+} // namespace impl
+
+// Writes the generated service interface into the given ZeroCopyOutputStream
+void GenerateService(
+    impl::protobuf::compiler::rust::Context &rust_generator_context,
+    const impl::protobuf::ServiceDescriptor *service);
+
+std::string GetRsGrpcFile(const impl::protobuf::FileDescriptor &file);
+} // namespace rust_grpc_generator
+
+#endif // NET_GRPC_COMPILER_RUST_GENERATOR_H_
